e680360d4303312677c905f36f993e7cc573981f
am0317-srmist <am0317@srmist.edu.in>
2026-03-26 09:11:45 +0530

    added Usecase 2

--- src/HelloApp.java
+++ src/HelloApp.java
@@ -2,4 +2,9 @@ public class HelloApp {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
+    public static void main(String[] args) {
+        String name = args[0];
+        System.out.println("Hello, " + name + "!");
+    }
+}
 }
