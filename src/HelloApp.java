46cf5905684ef021b90f066183e4805005b0add5
am0317-srmist <am0317@srmist.edu.in>
2026-04-01 23:40:50 +0530

    added Usecase 4

--- src/HelloApp.java
+++ src/HelloApp.java
@@ -1,13 +1,21 @@
 public class HelloApp {
+
     public static void main(String[] args) {
 
-
-        String name = "World";
+        StringBuilder names = new StringBuilder();
 
         if (args.length > 0) {
-            name = args[0]; 
+            for (int i = 0; i < args.length; i++) {
+                names.append(args[i]);
+
+                if (i < args.length - 1) {
+                    names.append(", ");
+                }
+            }
+        } else {
+            names.append("World");
         }
 
-        System.out.println("Hello, " + name + "!");
+        System.out.println("Hello, " + names.toString() + "!");
     }
 }
